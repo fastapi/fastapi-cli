07e96895327d57d1e682e0e5c5f79f6831787ba7
Sebastián Ramírez <tiangolo@gmail.com>
2024-05-02 13:36:52 -0700

    ✅ Tweak tests fixtures setup

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,7 +16,8 @@ def reset_syspath() -> Generator[None, None, None]:
 
 
 @pytest.fixture(autouse=True, scope="session")
-def set_terminal_width() -> None:
+def setup_terminal() -> None:
     rich_utils.MAX_WIDTH = 3000
+    rich_utils.FORCE_TERMINAL = False
     setup_logging(terminal_width=3000)
     return

2e241e3275ca1a2cec40993e108161012ba3694e
07pepa <pepe@wont_share.com>
2024-06-18 18:09:50 +0200

    add logging overriding

--- src/fastapi_cli/cli.py
+++ src/fastapi_cli/cli.py
@@ -7,6 +7,7 @@ from rich import print
 from rich.padding import Padding
 from rich.panel import Panel
 from typing_extensions import Annotated
+from uvicorn.config import LOGGING_CONFIG
 
 from fastapi_cli.discover import get_import_string
 from fastapi_cli.exceptions import FastAPICLIException
@@ -60,6 +61,7 @@ def _run(
     command: str,
     app: Union[str, None] = None,
     proxy_headers: bool = False,
+    log_config: Union[Path, None] = None,
 ) -> None:
     try:
         use_uvicorn_app = get_import_string(path=path, app_name=app)
@@ -89,6 +91,7 @@ def _run(
         raise FastAPICLIException(
             "Could not import Uvicorn, try running 'pip install uvicorn'"
         ) from None
+
     uvicorn.run(
         app=use_uvicorn_app,
         host=host,
@@ -97,6 +100,8 @@ def _run(
         workers=workers,
         root_path=root_path,
         proxy_headers=proxy_headers,
+        # fallback to default uvicorn log config if nothing is provided
+        log_config=LOGGING_CONFIG if not log_config else str(log_config),
     )
 
 
@@ -145,6 +150,12 @@ def dev(
             help="Enable/Disable X-Forwarded-Proto, X-Forwarded-For, X-Forwarded-Port to populate remote address info."
         ),
     ] = True,
+    log_config: Annotated[
+        Union[Path, None],
+        typer.Option(
+            help="Logging configuration file. Supported formats: .ini, .json, .yaml. be tried."
+        ),
+    ] = None,
 ) -> Any:
     """
     Run a [bold]FastAPI[/bold] app in [yellow]development[/yellow] mode. 🧪
@@ -180,6 +191,7 @@ def dev(
         app=app,
         command="dev",
         proxy_headers=proxy_headers,
+        log_config=log_config,
     )
 
 
@@ -234,6 +246,12 @@ def run(
             help="Enable/Disable X-Forwarded-Proto, X-Forwarded-For, X-Forwarded-Port to populate remote address info."
         ),
     ] = True,
+    log_config: Annotated[
+        Union[Path, None],
+        typer.Option(
+            help="Logging configuration file. Supported formats: .ini, .json, .yaml."
+        ),
+    ] = None,
 ) -> Any:
     """
     Run a [bold]FastAPI[/bold] app in [green]production[/green] mode. 🚀
@@ -270,6 +288,7 @@ def run(
         app=app,
         command="run",
         proxy_headers=proxy_headers,
+        log_config=log_config,
     )
 
 

fb946a349628ffb0239650942afb8be223b3a677
Matthew Messinger <mattmess1221@gmail.com>
2024-10-08 21:34:01 -0400

    tests: fix imports not being cleaned up

--- tests/assets/default_files/default_main/api.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-
-@app.get("/")
-def app_root():
-    return {"message": "single file app"}
--- tests/conftest.py
+++ tests/conftest.py
@@ -21,3 +21,12 @@ def setup_terminal() -> None:
     rich_utils.FORCE_TERMINAL = False
     setup_logging(terminal_width=3000)
     return
+
+
+@pytest.fixture(autouse=True)
+def reset_imports():
+    modules = sys.modules.copy()
+    try:
+        yield
+    finally:
+        sys.modules = modules

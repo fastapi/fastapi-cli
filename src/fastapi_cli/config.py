4a271553359e6d55063a60d7d51b6217770f01f6
Patrick Arminio <patrick.arminio@gmail.com>
2025-12-22 17:52:32 +0100

    ➖ Drop support for Pydantic v1 (#268)

--- src/fastapi_cli/config.py
+++ src/fastapi_cli/config.py
@@ -3,12 +3,8 @@ from pathlib import Path
 from typing import Any, Dict, Optional
 
 from pydantic import BaseModel, StrictStr
-from pydantic.version import VERSION as PYDANTIC_VERSION
 
 logger = logging.getLogger(__name__)
-
-PYDANTIC_VERSION_MINOR_TUPLE = tuple(int(x) for x in PYDANTIC_VERSION.split(".")[:2])
-PYDANTIC_V2 = PYDANTIC_VERSION_MINOR_TUPLE[0] == 2
 
 
 class FastAPIConfig(BaseModel):
@@ -43,8 +39,4 @@ class FastAPIConfig(BaseModel):
         if entrypoint is not None:
             config["entrypoint"] = entrypoint
 
-        # Pydantic v2 uses model_validate, v1 uses parse_obj
-        if not PYDANTIC_V2:
-            return cls.parse_obj(config)  # type: ignore[no-any-return, unused-ignore]
-
-        return cls.model_validate(config)  # type: ignore[no-any-return, unused-ignore, attr-defined]
+        return cls.model_validate(config)
